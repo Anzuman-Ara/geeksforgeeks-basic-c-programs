4dc9ba94aa8822fcf0514dfdf000e23e98ff7618
Anzuman-Ara <120357885+Anzuman-Ara@users.noreply.github.com>
2023-08-30 00:41:04 +0600

    C Program to Print an Integer Entered By the User

---
+++ reading and then displaying an integer value.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+
+    int integer;
+     //asking the user to provide an integer number
+     printf("Enter an integer number :");
+
+    //taking an integer as input from the user
+      scanf("%d",&integer);
+
+   //output function for printing the entered integer
+   printf("The integer number that you entered is %d",integer);
+
+
+}
+
+
