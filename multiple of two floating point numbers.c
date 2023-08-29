aecc38571ff32e23801c359cdc8f12f5b760d742
Anzuman-Ara <120357885+Anzuman-Ara@users.noreply.github.com>
2023-08-30 01:09:57 +0600

    C Program to Multiply two Floating-Point Numbers

---
+++ multiple of two floating point numbers.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+main(){
+
+    float input_1,input_2;
+     //asking the user to provide two floating point numbers
+     printf("Enter any two floating point numbers :");
+
+    //taking the two floating point numbers as input from the user
+      scanf("%f%f",&input_1,&input_2);
+
+    //showing the result of multiplication of these two floating point numbers
+     printf("The multiplication of these two floating points is : %f",input_1*input_2);
+
+
+    }
+
